93373480ba03b7e85a1a2e73b5edc726899a2ba6
beaexpert <beaexpert.lda@gmail.com>
2024-09-09 15:54:26 +0100

    Add project files.

---
+++ AmaranteManager/MongoConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace webapi_amarante_all.AmaranteManager
+{
+    public class MongoConfig
+    {
+        public string? SacanaCollectionName { get; set; }
+        public string? ConnectionString { get; set; }
+        public string? DatabaseName { get; set; }
+    }
+}
---
+++ AmaranteManager/Sacana.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace webapi_amarante_all.AmaranteManager
+{
+    public class Sacana
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        
+        public string? Nome { get; set; }
+
+        public string? Apelido { get; set; }
+    }
+}
